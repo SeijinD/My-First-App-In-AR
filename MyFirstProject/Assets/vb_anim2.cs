857b0f76284e047d10f8092d5d632b39405658c2
George Kara <seijindemon@protonmail.com>
2020-01-04 16:45:47 +0200

    add project

---
+++ MyFirstProject/Assets/vb_anim2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim2 : MonoBehaviour, IVirtualButtonEventHandler {
+	
+	public Animator skeleton_Anim;
+	public GameObject skeleton_Object;
+	
+	// Use this for initialization
+	void Start () {
+		skeleton_Object = GameObject.Find("VirtualButton2");
+		skeleton_Object.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		skeleton_Anim.GetComponent<Animator>();
+	}
+	
+	public void OnButtonPressed(VirtualButtonBehaviour vb){
+		skeleton_Anim.Play("skeleton_Animation2");
+		Debug.Log("BTN Pressed");
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+		skeleton_Anim.Play("none");
+		Debug.Log("BTN Pressed");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
